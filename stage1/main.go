ddcd5864eed9a7dd194d11a515a95ed2f2b41c1d
Hermann Rösch <mr.guty@Hotmail.com>
2022-07-27 10:27:09 -0500

    Added stage pre-requisites for Stage #1

--- stage1/main.go
+++ stage1/main.go
@@ -1,5 +1,13 @@
 package main
 
+/*
+[Smart Calculator - Stage 1/7: 2+2](https://hyperskill.org/projects/74/stages/409/implement)
+-------------------------------------------------------------------------------
+[Primitive types](https://hyperskill.org/learn/topic/1807)
+[Input/Output](https://hyperskill.org/learn/topic/1506)
+[Arithmetic operations](https://hyperskill.org/learn/topic/1785)
+*/
+
 import "fmt"
 
 func main() {
